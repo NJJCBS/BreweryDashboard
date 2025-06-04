beba3d93e5a9b0d71897422f208343816005488a
NJJCBS <nick@onedropbrewingco.com.au>
2025-06-04 17:35:49 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
+// pages/_app.js
 import Head from 'next/head'
-import '../styles/globals.css'
+import '../styles/globals.css'   // ← this file now exists
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* If you used favicon.png instead of favicon.ico, change to /favicon.png */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
