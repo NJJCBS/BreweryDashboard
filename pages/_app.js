c21911c8a5ae8228026f5b2e9bb93b1eb3cc5dd2
NJJCBS <nick@onedropbrewingco.com.au>
2025-06-04 17:32:11 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
-// pages/_app.js
 import Head from 'next/head'
-import '../styles/globals.css'     // keep this if you already have a global CSS file
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* 
-          If you used "favicon.png" instead of .ico, change this line to:
-            <link rel="icon" href="/favicon.png" type="image/png" />
-          Otherwise leave it as /favicon.ico
-        */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
